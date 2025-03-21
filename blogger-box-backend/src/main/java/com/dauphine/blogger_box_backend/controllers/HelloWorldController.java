7b1a124a2b2fb9d6b284319cacfcc2a2afda558d
victoriengb <victoriengbaguidi@gmail.com>
2025-03-21 11:44:25 +0100

    feature: Implemented the first 3 endpoints

---
+++ blogger-box-backend/src/main/java/com/dauphine/blogger_box_backend/controllers/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.dauphine.blogger_box_backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+    
+    @GetMapping("hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+    
+    @GetMapping("hello")
+    public String helloByName(@RequestParam String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+}
