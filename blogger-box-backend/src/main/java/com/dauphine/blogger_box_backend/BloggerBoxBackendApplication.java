02ff7b859052790aedeb52de91f1ed8b2cc79de4
victoriengb <victoriengbaguidi@gmail.com>
2025-04-04 09:46:05 +0200

    feature: Added App and API documentation on Swagger

--- blogger-box-backend/src/main/java/com/dauphine/blogger_box_backend/BloggerBoxBackendApplication.java
+++ blogger-box-backend/src/main/java/com/dauphine/blogger_box_backend/BloggerBoxBackendApplication.java
@@ -3,7 +3,17 @@ package com.dauphine.blogger_box_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Blogger Box backend",
+		version = "1.0",
+		description = "Blogger Box endpoints and api"
+	)
+)
 public class BloggerBoxBackendApplication {
 
 	public static void main(String[] args) {
--- blogger-box-backend/src/main/java/com/dauphine/blogger_box_backend/controllers/HelloWorldController.java
+++ blogger-box-backend/src/main/java/com/dauphine/blogger_box_backend/controllers/HelloWorldController.java
@@ -5,21 +5,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
+@Tag(
+        name = "Hello World API",
+        description = "Hello World endpoints"
+)
 public class HelloWorldController {
     
     @GetMapping("hello-world")
+    @Operation(
+            summary = "Hello World",
+            description = "Returns a hello world message"
+    )
     public String helloWorld() {
         return "Hello World!";
     }
     
+    //For filtering
     @GetMapping("hello")
+    @Operation(
+            summary = "Hello by name",
+            description = "Returns a hello message with the given name"
+    )
     public String helloByName(@RequestParam String name) {
         return "Hello " + name + "!";
     }
 
+    //To get a subresource
     @GetMapping("hello/{name}")
+    @Operation(
+            summary = "Hello by name",
+            description = "Returns a hello message with the given name"
+    )
     public String hello(@PathVariable String name) {
         return "Hello " + name + "!";
     }
